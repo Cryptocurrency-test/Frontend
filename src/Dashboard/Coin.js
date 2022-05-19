4375f71cc065b7bb474a938f85ea58655f58ee36
MahalingamVS <76953836+MahalingamVS@users.noreply.github.com>
2022-05-19 21:41:58 +0530

    Update Coin.js

--- src/Dashboard/Coin.js
+++ src/Dashboard/Coin.js
@@ -8,22 +8,22 @@ const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap }) =>
         <div className='coin-container'>
             <div className="coin-row">
                 <div className="coin">
-                    <img src={image} alt="crypto"/>
+                    {/* <img src={image} alt="crypto"/> */}
                     <h1>{name}</h1>
                     <p className="coin-symbol">{symbol}</p>
                 </div>
                 <div className="coin-data">
                     <p className="coin-price">${price}</p>
-                    <p className="coin-volume">${volume.toLocaleString()}</p>  
+                    {/* <p className="coin-volume">${volume.toLocaleString()}</p>   */}
                     {/*  toLocaleString places commas in the values */}
-                    {priceChange < 0 ? (
+                    {/* {priceChange < 0 ? (
                         <p className="coin-percent red">{priceChange}%</p>
                     ) : (
                         <p className="coin-percent green">{priceChange}%</p>
                     )}
                     <p className="coin-marketcap">
                         Mkt Cap: ${marketcap.toLocaleString()}
-                    </p>
+                    </p> */}
                 </div>
             </div>
         </div>
